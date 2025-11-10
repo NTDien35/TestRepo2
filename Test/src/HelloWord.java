825ca4f8d9a761841541e7e67c325f707ffc01a5
Nguyễn Toàn Diện <ntdien35@gmail.com>
2025-11-10 21:40:31 +0700

    changbranch

--- Test/src/HelloWord.java
+++ Test/src/HelloWord.java
@@ -1,6 +1,6 @@
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("Hello Everyone");
-        System.out.println("New change");
+        System.out.println("Change form new branch");
     }
 }
